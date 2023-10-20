8cb22fd22d270163c3b978c286959f4746542157
Yuri Simeone <y.simeone@nesea.it>
2023-10-20 11:12:50 +0200

    Aggiornamenti

--- script.js
+++ script.js
@@ -4,11 +4,33 @@ function getNextActivityLink() {
     return $('#next-activity-link').attr('href');
 }
 
+function initFasceOrarie() {
+    setInterval(() => {
+        try{
+            const currentDate = new Date();
+            const hours = currentDate.getHours();
+            const minutes = currentDate.getMinutes();
+
+            const isMorning = (hours > 9 && minutes)
+
+        } catch {}
+    }, 60000)
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 function init() {
     let nextActivityLink = getNextActivityLink();
 
     if(nextActivityLink){
-        window.location.href = nextActivityLink;
+        setTimeout(() => {
+            window.location.href = nextActivityLink;
+        }, getRandomInt(5,10))
+
     } else {
         const interval = setInterval(() => {
             try{
