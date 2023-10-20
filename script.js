153004f1a478c04cb72538ce033e9b73b9dea780
Yuri Simeone <y.simeone@nesea.it>
2023-10-20 12:59:31 +0200

    Fasce orarie ottimizzate

--- script.js
+++ script.js
@@ -11,62 +11,65 @@ function getDateInMillis(hours, minutes) {
     return date
 }
 
+function isCorrectFasciaOraria() {
+    const currentTime = Date.now();
+
+    const fineLavoro = getDateInMillis(17, 55);
+    const inizioLavoro = getDateInMillis(9, 5)
+
+    const pausaPranzoStart = getDateInMillis(12, 55);
+    const pausaPranzoEnd = getDateInMillis(14, 5);
+
+    if (currentTime < inizioLavoro) {
+        //è mattina, mettiamo in pausa
+        return false;
+    } else if (currentTime > fineLavoro) {
+        //è sera, mettiamo in pausa
+        return false
+    } else if (currentTime >= pausaPranzoStart && currentTime <= pausaPranzoEnd) {
+        // è pausa pranzo, mettiamo in pausa
+        return false;
+    }
+
+    return true;
+}
+
 function initFasceOrarie() {
     let isVideoPaused = false;
     setInterval(() => {
 
-        try{
-            const currentTime = Date.now();
-
-            const fineLavoro = getDateInMillis(17,55);
-            const inizioLavoro = getDateInMillis(9,5)
-
-            const pausaPranzoStart = getDateInMillis(12,55);
-            const pausaPranzoEnd = getDateInMillis(14,5);
-
-            if(currentTime < inizioLavoro){
-                //è mattina, mettiamo in pausa
+        if (!isCorrectFasciaOraria()) {
+            if (!isVideoPaused) {
                 player.pause();
                 isVideoPaused = true;
             }
-            else if(currentTime > fineLavoro){
-                //è sera, mettiamo in pausa
-                player.pause();
-                isVideoPaused = true;
+        } else {
+            if (isVideoPaused) {
+                player.play();
+                isVideoPaused = false;
             }
-            else if(currentTime >= pausaPranzoStart && currentTime <= pausaPranzoEnd){
-                // è pausa pranzo, mettiamo in pausa
-                player.pause();
-                isVideoPaused = true;
-            } else {
-                if(isVideoPaused){
-                    player.play();
-                    isVideoPaused = false;
-                }
-            }
+        }
 
-
-        } catch {}
     }, 60000)
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+function getRandomMs(min, max) {
+    min = Math.ceil(min * 1000);
+    max = Math.floor(max * 1000);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
 function init() {
     let nextActivityLink = getNextActivityLink();
 
-    if(nextActivityLink){
+    if (nextActivityLink) {
         setTimeout(() => {
             window.location.href = nextActivityLink;
-        }, getRandomInt(5,10))
+        }, getRandomMs(5, 10))
 
     } else {
         const interval = setInterval(() => {
-            try{
+            try {
                 player = new Vimeo.Player($('iframe')[0]);
 
                 player.play();
@@ -75,12 +78,12 @@ function init() {
                     console.log('video finito, vado al prossimo')
                     setTimeout(() => {
                         nextActivityLink = getNextActivityLink();
-                        if(nextActivityLink){
+                        if (nextActivityLink) {
                             window.location.href = nextActivityLink;
                         } else {
                             window.location.reload();
                         }
-                    }, getRandomInt(5,15))
+                    }, getRandomMs(5, 15))
                 })
 
                 initFasceOrarie();
@@ -88,13 +91,15 @@ function init() {
             } catch {
 
             }
-        }, getRandomInt(5,8))
+        }, getRandomMs(5, 8))
     }
-
 }
 
-
-
-$(window).on('load',() => {
+$(window).on('load', () => {
     init();
+    const interval = setInterval(() => {
+        if (isCorrectFasciaOraria()) {
+            clearInterval(interval)
+        }
+    }, 1000)
 })
